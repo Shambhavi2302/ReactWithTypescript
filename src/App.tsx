b24e04324babd0de6acee7af07b527667098df05
Shambhavi Swami <99818052+Shambhavi2302@users.noreply.github.com>
2024-10-14 16:27:18 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import { Counter } from './redux/counter/Counter';
+import Counter from "./redux/counter/Counter";
+import ExampleComponent from "./axiosInstance/ExampleComponent";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/axios" element={<ExampleComponent />} />
+          <Route path="*" element={<NoPage />} />
+          <Route path="/counter" element={<Counter />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+export default App;
