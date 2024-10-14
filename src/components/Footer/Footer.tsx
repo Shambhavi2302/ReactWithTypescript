7615629a9296d50fb24962cb5405ef7fdd11e203
ThriveniDasari-dev <dasarithriveni009@gmail.com>
2024-10-14 19:03:26 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+  return (
+      <div className="Footer">
+        <div>
+          <a href="#">All right reserve @ Capgemini DCX WebUI 2024</a>
+        </div>
+      </div>
+  );
+}
+export default Footer;
